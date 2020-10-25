3c15227234d15cc4cb1fcc4d445d021eeb2e355d
Julieta Salvadó <julieta.salvado@gmail.com>
2020-10-24 21:52:46 -0300

    Add two columns

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Calendar from "./components/Calendar"
+import TopicSection from "./components/TopicSection"
 import Status from "./components/Status"
+import { Grid, Segment } from 'semantic-ui-react'
 
 class App extends Component {
 
@@ -18,7 +20,16 @@ class App extends Component {
                     </div>
                 </header>
                 <main>
-                    <Calendar/>
+                    <Segment>
+                        <Grid columns={2} relaxed='very'>
+                            <Grid.Column>
+                                <Calendar/>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <TopicSection/>
+                            </Grid.Column>
+                        </Grid>
+                    </Segment>
                     <div className="footer">
                         <Status/>
                     </div>
---
+++ src/components/TopicSection.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+import { Segment } from 'semantic-ui-react'
+
+class TopicSection extends Component {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <Segment>Pellentesque habitant morbi tristique senectus.</Segment>
+        );
+    }
+
+}
+
+export default  TopicSection
