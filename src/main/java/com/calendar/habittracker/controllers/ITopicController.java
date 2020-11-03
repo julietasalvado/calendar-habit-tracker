695cd34e14c63482e4989bea62624346e2c46edb
Julieta Salvadó <julieta.salvado@gmail.com>
2020-11-02 23:54:31 -0300

    Add get all topics endpoints

---
+++ src/main/java/com/calendar/habittracker/controllers/ITopicController.java
@@ -0,0 +1,9 @@
+package com.calendar.habittracker.controllers;
+
+import com.calendar.habittracker.model.Topic;
+
+import java.util.List;
+
+public interface ITopicController {
+  List<Topic> getAllTopics();
+}
---
+++ src/main/java/com/calendar/habittracker/controllers/TopicController.java
@@ -0,0 +1,22 @@
+package com.calendar.habittracker.controllers;
+
+import com.calendar.habittracker.model.Topic;
+import com.calendar.habittracker.services.TopicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TopicController implements ITopicController {
+  
+  private final TopicService topicService;
+  
+  @GetMapping("/topics")
+  public List<Topic> getAllTopics() {
+    return topicService.getAllTopics();
+  }
+}
