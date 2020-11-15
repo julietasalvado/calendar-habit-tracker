51fd2423254e9716c9648d8c408cd9d7574a2309
Julieta Salvadó <julieta.salvado@gmail.com>
2020-11-14 22:28:47 -0300

    Add topic endpoint

---
+++ src/main/java/com/calendar/habittracker/model/AddTopicRequest.java
@@ -0,0 +1,8 @@
+package com.calendar.habittracker.model;
+
+import lombok.Data;
+
+@Data
+public class AddTopicRequest {
+    private String title;
+}
--- src/main/java/com/calendar/habittracker/model/Topic.java
+++ src/main/java/com/calendar/habittracker/model/Topic.java
@@ -1,15 +1,75 @@
 package com.calendar.habittracker.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "topics")
 public class Topic {
   @Id
   private String id;
   private String title;
-  private Integer level;
-  private Integer completed;
+  private int level = 1;
+  private int completed = 0;
+  private ArrayList resources;
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public Builder toBuilder() {
+    return new Builder().id(this.id).title(this.title).level(this.level).completed(this.completed).resources(this.resources);
+  }
+
+  public static class Builder {
+    private String id;
+    private String title;
+    private int level;
+    private int completed;
+    private ArrayList resources;
+
+    Builder() {
+      this.level = 1;
+    }
+
+    public Builder id(String id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder title(String title) {
+      this.title = title;
+      return this;
+    }
+
+    public Builder level(int level) {
+      this.level = level;
+      return this;
+    }
+
+    public Builder completed(int completed) {
+      this.completed = completed;
+      return this;
+    }
+
+    public Builder resources(ArrayList resources) {
+      this.resources = resources;
+      return this;
+    }
+
+    public Topic build() {
+      return new Topic(id, title, level, completed, resources);
+    }
+
+    public String toString() {
+      return "Topic.Builder(id=" + this.id + ", title=" + this.title + ", level=" + this.level + ", completed=" + this.completed + ", resources=" + this.resources + ")";
+    }
+  }
 }
