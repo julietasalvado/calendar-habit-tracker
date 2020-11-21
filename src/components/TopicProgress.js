a576663b994ac0593c228b632bceeffb72c38d0c
Julieta Salvadó <julieta.salvado@gmail.com>
2020-11-21 15:44:48 -0300

    Remove topicprogress constructor

--- src/components/TopicProgress.js
+++ src/components/TopicProgress.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { Progress } from 'semantic-ui-react'
 
 class TopicProgress extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return <Progress percent={this.props.progress} indicating/>;
     }
